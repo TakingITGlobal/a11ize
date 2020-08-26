67d6bdb5bd8844fc26bdd932ea193a6f80eeab0e
Jason Huang <jasonhuang20035@gmail.com>
2020-08-26 15:03:17 -0400

    more updates

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,9 +5,10 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-knobs/register'],
   webpackFinal: (config) => {
+    console.log(custom)
     return {
       ...config,
-      module: { ...config.module, rules: custom.module.rules },
+      module: { ...config.module, rules: custom[0].module.rules },
     };
   },
 };
