a2b8afc2dbca78147b1d1a8abb53b90e708f043a
Jason Huang <jasonhuang20035@gmail.com>
2020-08-27 12:44:03 -0400

    feat: Add Multi Language Support

--- src/no-react.js
+++ src/no-react.js
@@ -4,11 +4,10 @@ import 'regenerator-runtime/runtime.js';
 import getBundleURL from './getBundleUrl';
 // wrapper goes here
 // compile to dist/no-react.js using babel
-// ok what tf do we put here
 
 import A11yWrapper from './components/A11yWrapper';
 
-console.log('asdf');
+const language = navigator.language.split('-')[0];
 
 const wrapper = document.getElementById('accessible-content');
 const children = wrapper.innerHTML;
@@ -23,7 +22,7 @@ console.log(`Served from ${getBundleURL()}`);
 if (wrapper) {
   console.log('Wrapper found! Loading button...');
   ReactDOM.render(
-    <A11yWrapper dangerouslySet={children} id="a11y-module" />,
+    <A11yWrapper dangerouslySet={children} language={language} id="a11y-module" />,
     wrapper
   );
 }
