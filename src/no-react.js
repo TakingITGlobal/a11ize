4d2e3b82e54d6d4d32d6b51d060c7fe0eaddee8d
Jason Huang <jasonhuang20035@gmail.com>
2020-08-28 10:19:08 -0400

    fix: restore regenerator-runtime import
    The no-react build fails to load things without regenerator
    
    Co-authored-by: David Li <david.li3100@gmail.com>

--- src/no-react.js
+++ src/no-react.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import getBundleURL from './getBundleUrl';
 import A11yWrapper from './components/A11yWrapper';
+import 'regenerator-runtime/runtime.js';
 
 const language = navigator.language.split('-')[0];
 
